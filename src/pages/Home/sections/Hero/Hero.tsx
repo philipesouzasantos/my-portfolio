45f942d57af66815db6c9d4b19813d70f5895383
philipesouzasantos <souzasiqueira90@gmail.com>
2024-11-14 12:46:08 -0300

    refactor: Hero page

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,31 +1,52 @@
-import { styled } from "@mui/material"
+import { Button, Container, styled, Typography } from "@mui/material"
 import Grid from '@mui/material/Grid2';
-import Avatar from '../../../../assets/images/avatar.jpeg'
+import Avatar from '../../../../assets/images/avatar.jpeg';
+import DownloadIcon from '@mui/icons-material/Download';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 const Hero = () => {
 
     const StyledHero = styled("div")(()=> ({
-        backgroundColor: "black"
+        backgroundColor: "black",
+        height:"100vh"
     }))
 
     const StyledImg = styled("img")(()=> ({
-        width: "30%",
+        width: "100%",
         borderRadius: "50%"
     }))
+
+// obs: Container permite melhorar bastante a 
+// responsividade mesmo em telas muito grandes
 
     return (
       <>
         <StyledHero>
-        <Grid container spacing={2}>
-            <Grid size={4}>
-                <StyledImg src={Avatar} />
-            </Grid>
-            <Grid size={8}>
-                size=4
-            </Grid>
-        </Grid>
-
-        
+            <Container>
+                <Grid container spacing={2}>
+                    <Grid size={{ xs: 12, md: 4 }}>
+                        <StyledImg src={Avatar} />
+                    </Grid>
+                    <Grid size={{ xs: 12, md: 8 }}>
+                        <Typography color="primary" variant="h1" textAlign="center">Philipe Siqueira</Typography>
+                        <Typography color="primary" variant="h2" textAlign="center">Business Intelligence Developer</Typography>
+                        <Grid container display={"flex"} justifyContent={"center"}>
+                            <Grid size={{ xs: 12, md: 6 }} display={"flex"} justifyContent={"center"}>
+                                <Button>
+                                    <DownloadIcon/>
+                                    Download CV
+                                </Button>
+                            </Grid>
+                            <Grid size={{ xs: 12, md: 6  }} display={"flex"} justifyContent={"center"}>
+                                <Button>
+                                    <MailOutlineIcon/>
+                                    Contact me
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Container>
         </StyledHero>
       </>
     )
